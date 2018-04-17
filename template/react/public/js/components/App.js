2bf9cba2603361b4822bc8c5f81f2a90fd79e084
mantis <158673384@qq.com>
2018-04-17 19:51:16 +0800

    引入热更新模块，懒加载路由，修改express配置

--- template/react/public/js/components/App.js
+++ template/react/public/js/components/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
+import { hot } from 'react-hot-loader'
+import Loadable from 'react-loadable'
 
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-import Content from './Content'
+
+// 公共加载动画组件
+const Loading = () => (
+    <div>请替换成各自的加载动画</div>
+)
+
+// 异步加载路由
+const LoadContent = Loadable({
+    loader: () => import('./Content'),
+    loading: Loading
+})
 
 class App extends Component {
     render () {
         return [
             <Header key='header' />,
-            <Route key='rootPath' exact path='/' render={ () => <Redirect from='/' to='/content' push={ false } /> } />,
-            <Route key='content' path='/content' component={ Content } />,
-            <Footer key='footer' />
+            <Switch key='section'>
+                <Route path="/content" component={ LoadContent } />
+                <Redirect from="/" to="/content" push={ false } />
+            </Switch>,
+            <Footer key='footer' />,
         ]
     }
 }
 
-export default App
+export default hot(module)(App)
--- template/react/public/js/components/Content.js
+++ template/react/public/js/components/Content.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
 
 class Content extends Component {
     render () {
